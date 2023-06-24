34b3ada2e6d2783f34f931e4df04adf8ff896439
reenphygeorge <reenphygeorge@gmail.com>
2023-06-24 12:22:15 +0530

    feat: added cmd args

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,22 +15,35 @@ import (
 )
 
 func main() {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-interrupt
-		logger.Exit()
-		time.Sleep(500 * time.Millisecond)
-		os.Exit(0)
-	}()
+	args := os.Args
 
-	rootPath := "."
-	var configObject config.Config
-	
-	config.GetValues(&configObject)
-	logger.StaringServer()
-	vistedPath := path.GetFilePaths(rootPath, configObject.SkipDirectories,0)
-	htmlFiles := path.GetFilePaths(rootPath, configObject.SkipDirectories,1)
-	go watch.WatchFiles(vistedPath, &htmlFiles, configObject, rootPath)
-	server.Server(configObject.Port, &htmlFiles)
+	if len(args) == 1 { 
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-interrupt
+			logger.Exit()
+			time.Sleep(500 * time.Millisecond)
+			os.Exit(0)
+		}()
+
+		rootPath := "."
+		var configObject config.Config
+		
+		config.GetValues(&configObject)
+		logger.StaringServer()
+		vistedPath := path.GetFilePaths(rootPath, configObject.SkipDirectories,0)
+		htmlFiles := path.GetFilePaths(rootPath, configObject.SkipDirectories,1)
+		go watch.WatchFiles(vistedPath, &htmlFiles, configObject, rootPath)
+		server.Server(configObject.Port, &htmlFiles)	
+	} else {
+		for i := 1; i < len(args); i++ {
+			arg := args[i]
+			if arg == "-v" || arg == "--version" {
+				fmt.Println("Servette version 1.0.0")
+			} else {
+				fmt.Println("unknown option: ", arg)
+			}
+		}
+	}
 }
